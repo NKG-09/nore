7951bba81bb64a77664a6c5600cc4708b6301a32
NKG-09 <200639623+NKG-09@users.noreply.github.com>
2025-11-06 23:38:25 +0530

    Setup script and added function to create tasks

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-document.querySelector();
+const incompleteTasksDisplay = document.querySelector("#tasks .incomplete");
+const completeTasksDisplay = document.querySelector("#tasks .complete");
+
+document.addEventListener("click", () => {
+  createTask(
+    "Finish English homework",
+    "Due date: 31/11/2025",
+    "Make sure to finish trigonometric equations",
+  );
+});
+
+function createTask (headingText, ...paras) {
+  const task = document.createElement("div");
+  task.classList.add("task");
+  
+  const heading = document.createElement("h2");
+  heading.textContent = headingText;
+
+  const paraList = document.createElement("ul");
+  
+  paras.forEach((para) => {
+    const li = document.createElement("li");
+    li.textContent = para;
+    paraList.appendChild(li);
+   });
+
+  task.appendChild(heading);
+  task.appendChild(paraList);
+
+  incompleteTasksDisplay.appendChild(task);
+}
