3b75f2ce432b3989440d20a81a9744c5fe63de97
NKG-09 <200639623+NKG-09@users.noreply.github.com>
2025-11-07 18:24:11 +0530

    Add proper UI buttons for completing, deleting, and incompleting tasks

--- script.js
+++ script.js
@@ -22,18 +22,6 @@ document.addEventListener("keyup", (e) => {
   }
 });
 
-document.addEventListener("keyup", (e) => {
-  if (e.key === "w" && incompleteTasksDisplay.childNodes.length) {
-    switchTask([...incompleteTasksDisplay.childNodes].at(0));
-  }
-  if (e.key === "e" && completeTasksDisplay.childNodes.length) {
-    switchTask([...completeTasksDisplay.childNodes].at(0));
-  }
-  if (e.key === "r" && incompleteTasksDisplay.childNodes.length) {
-    removeTask([...incompleteTasksDisplay.childNodes].at(0));
-  }
-});
-
 function createTask (headingText, ...paras) {
   const task = document.createElement("div");
   task.classList.add("task");
@@ -47,19 +35,35 @@ function createTask (headingText, ...paras) {
     const li = document.createElement("li");
     li.textContent = para;
     paraList.appendChild(li);
-   });
+  });
+
+  const buttonsContainer = document.createElement("div");
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", () => { removeTask(task) } );
+  buttonsContainer.appendChild(deleteButton);
+
+  const completeButton = document.createElement("button");
+  completeButton.textContent = "Complete";
+  completeButton.addEventListener("click", switchTask);
+  buttonsContainer.appendChild(completeButton);
 
   task.appendChild(heading);
   task.appendChild(paraList);
+  task.appendChild(buttonsContainer);
 
   incompleteTasksDisplay.appendChild(task);
 }
 
-function switchTask (task) {
+function switchTask (e) {
+  const task = e.target.closest(".task");
   if (task.parentElement === incompleteTasksDisplay) {
+    task.querySelector("button:nth-child(2)").textContent = "Incomplete";
     completeTasksDisplay.appendChild(task);
   }
   else {
+    task.querySelector("button:nth-child(2)").textContent = "Complete";
     incompleteTasksDisplay.appendChild(task);
   }
 }
